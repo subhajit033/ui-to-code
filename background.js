29245c6d3dd2322971a6bd89d7c7c62b8acb2b60
Subhajit Kundu <kundusubhajit73@gmail.com>
2025-04-13 12:15:29 +0530

    fixed triggering url

--- background.js
+++ background.js
@@ -10,14 +10,14 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 function sendToVSCode(componentInfo) {
   // Option 1: Use custom URL protocol to communicate with VS Code
   const jsonData = encodeURIComponent(JSON.stringify(componentInfo));
-  const vsCodeUrl = `vscode://ui-to-code-navigator/find?data=${jsonData}`;
+  const vsCodeUrl = `vscode://subhajitkundu.ui-to-code-generator?data=${jsonData}`;
 
   // Open the URL which will be handled by the VS Code extension
   chrome.tabs.create({ url: vsCodeUrl, active: false }, (tab) => {
     // Close the tab after a moment - it's just for triggering the protocol
-    setTimeout(() => {
-      chrome.tabs.remove(tab.id);
-    }, 500);
+    // setTimeout(() => {
+    //   chrome.tabs.remove(tab.id);
+    // }, 500);
   });
 
   // Option 2: Use WebSockets (would need a WebSocket server)
